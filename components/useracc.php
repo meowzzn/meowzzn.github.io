09fa706b42f2e9424576688f289dcf1f8fb6429d
Nichoy <139543780+meowzzn@users.noreply.github.com>
2023-07-29 20:59:41 +0800

    Added project files

---
+++ components/useracc.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); // Start the session if not already started
+
+$connection = mysqli_connect("localhost", "root", "", "brainybots");
+
+if (!$connection) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_SESSION['id'])) {
+    $userId = $_SESSION['id'];
+
+    $sql = "SELECT username FROM accounts WHERE id = $userId";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $username = $row['username'];
+    } else {
+        // Handle the case where the user ID is not found in the database
+        // For example, redirect the user to a login page.
+    }
+}
+
+$conn->close();
+?>
